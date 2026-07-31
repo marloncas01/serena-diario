21cb78e33d7d95f23e2f117feff903371a077ae4
Marlon Castillo <jaimerodriguezdiaztaller@gmail.com>
2026-07-31 02:21:38 -0500

    chore: prepare release v1.0 beta

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,17 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:serena_diario/main.dart';
 
 void main() {
   testWidgets('Muestra la bienvenida de Serena', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
     await initializeDateFormatting('es_ES');
     await tester.pumpWidget(const SerenaApp());
     expect(find.text('Serena'), findsOneWidget);
     expect(find.text('Tu espacio seguro para entenderte.'), findsOneWidget);
+    await tester.pump(const Duration(seconds: 2));
+    await tester.pumpAndSettle();
   });
 }
